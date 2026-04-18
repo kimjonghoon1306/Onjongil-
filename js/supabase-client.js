ecfec46cc58c8d56d5090bd77bb3b5b43859d86f
kimjonghoon1306 <s965300111@gmail.com>
2026-04-19 03:40:36 +0900

    Add files via upload

---
+++ js/supabase-client.js
@@ -0,0 +1,5 @@
+/* Supabase 연결 */
+const SUPABASE_URL = 'https://uaclmashekaekchxhica.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_gPSp9qiO0tqEX446PZ-Urg_YKdur_9H';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
